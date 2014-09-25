72c2b37a072e55aab27f9e085c3658b73dca2971
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-24 23:46:31 -0500

    working sample extension

--- extension/javascripts/content.js
+++ extension/javascripts/content.js
@@ -1,1 +1,19 @@
-console.log("starting content process");
+/*Handle requests from background*/
+function handleRequest(request, sender, sendResponse) {
+	if (request.callFunction == "toggleSidebar") toggleSidebar();
+}
+chrome.extension.onRequest.addListener(handleRequest);
+
+function toggleSidebar() {
+	var id = 'context-sidebar';
+
+	if ($('body').find('#' + id).length === 0) {
+		var $sidebar = $('<div id="' + id + '"></div>');
+		$sidebar.html("<h1>Hello World</h1>");
+		$('body').css("margin-right", "350px");
+		$('body').prepend($sidebar);
+	} else {
+		$('body').css("margin-right", "0");
+		$('#' + id).remove();
+	}
+}
