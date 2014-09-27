fecefe95b9436b4b9b824c735e9005b77cc5a788
helin24 <helin.shiah@gmail.com>
2014-09-27 17:34:31 -0500

    starting on chat manager spec

--- server/spec/chat_manager_spec.rb
+++ server/spec/chat_manager_spec.rb
@@ -1,3 +1,28 @@
-# describe ChatManager do 
-	
-# end
+require_relative 'spec_helper'
+
+describe ChatManager do 
+
+	describe 'start' do 
+		before(:all) do 
+			@chat_manager = ChatManager.new
+			EM.run { @chat_manager.start(host: "0.0.0.0", port: 3001) }
+		end
+
+		after(:all) do 
+			EM.stop
+		end
+
+		it 'starts running' do 
+			
+			expect(@chat_manager.class).to eq(ChatManager)
+		end
+
+
+	end
+
+	describe 'setup_client' do 
+		xit "can add a client" do 
+
+		end
+	end
+end
