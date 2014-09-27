1b20eb92d892fe130d608e52153f3ccd35cf1d5a
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-27 13:52:39 -0500

    adding check for ajax

--- server/controllers/urls.rb
+++ server/controllers/urls.rb
@@ -1,10 +1,14 @@
 post '/urls/get_messages/:i' do
-	url = Url.find_by(link: params[:url])
-	if !url.nil?
-		content_type :json
-		url.messages.last(params[:i]).to_json
+	if request.xhr?
+		url = Url.find_by(link: params[:url])
+		if !url.nil?
+			content_type :json
+			url.messages.last(params[:i]).to_json
+		else
+			content_type :json
+			{}.to_json
+		end
 	else
-		content_type :json
-		{}.to_json
+		"Request not allowed."
 	end
 end
