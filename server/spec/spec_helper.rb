03e9311ba47daec43d993014f15571f78e848b72
helin24 <helin.shiah@gmail.com>
2014-09-28 11:15:59 -0500

    trivial chat manager test

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,3 +1,6 @@
 require_relative '../config/environment'
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection dbconfig['test']
+
+require 'em-websocket'
+require 'eventmachine'
