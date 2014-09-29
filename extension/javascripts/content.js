0f618db3d41c33162985594508f80cec94141126
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-29 11:15:00 -0500

    removing old code

--- extension/javascripts/content.js
+++ extension/javascripts/content.js
@@ -45,30 +45,6 @@ function adjustBodyPosition(command) {
 	} else if (command === 'close') {
 		$html.css("width", windowWidth);
 	}
-
-	// if (command === 'open') {
-	// 	if($body.css("position") === "absolute") {
-	// 		$body.css("right", "350px");
-	// 	} else if($body.css("position") === "relative") {
-	// 		$body.css("right", "350px");
-	// 	} else if($body.css("width") === ($(window).width() + "px")){
-	// 		$body.css("margin-right", "350px");
-	// 		$body.css("width", "auto");
-	// 	} else {
-	// 		$body.css("margin-right", "350px");
-	// 	}
-	// } else if (command === 'close') {
-	// 	if($body.css("position") === "absolute") {
-	// 		$body.css("right", "0");
-	// 	} else if($body.css("position") === "relative") {
-	// 		$body.css("right", "0");
-	// 	} else if($body.css("width") === "auto"){
-	// 		$body.css("margin-right", "0");
-	// 		$body.css("width", "100%");
-	// 	} else {
-	// 		$body.css("margin-right", "auto");
-	// 	}
-	// }
 }
 
 $(document).on("click", "#minimize-button", function() {
