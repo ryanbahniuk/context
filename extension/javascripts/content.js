5d43377560d76a3909c1a8f5d67a69cc587c10a0
Ryan Bahniuk <ryan.bahniuk@gmail.com>
2014-09-29 10:34:04 -0500

    changed adjustBodyPosition function and corrected SO issue

--- extension/javascripts/content.js
+++ extension/javascripts/content.js
@@ -7,6 +7,7 @@ function handleRequest(request, sender, sendResponse) {
 chrome.extension.onRequest.addListener(handleRequest);
 
 var open = false;
+var windowWidth = $(window).width();
 
 function toggleSidebar() {
 	var id = 'iframe-wrapper';
@@ -25,31 +26,38 @@ function toggleSidebar() {
 }
 
 function adjustBodyPosition(command) {
+	var $html = $('html');
 	var $body = $('body');
 
 	if (command === 'open') {
-		if($body.css("position") === "absolute") {
-			$body.css("right", "350px");
-		} else if($body.css("position") === "relative") {
-			$body.css("right", "350px");
-		} else if($body.css("width") === ($(window).width() + "px")){
-			$body.css("margin-right", "350px");
-			$body.css("width", "auto");
-		} else {
-			$body.css("margin-right", "350px");
-		}
+		$html.css("width", windowWidth - 350 + "px");
 	} else if (command === 'close') {
-		if($body.css("position") === "absolute") {
-			$body.css("right", "0");
-		} else if($body.css("position") === "relative") {
-			$body.css("right", "0");
-		} else if($body.css("width") === "auto"){
-			$body.css("margin-right", "0");
-			$body.css("width", "100%");
-		} else {
-			$body.css("margin-right", "auto");
-		}
+		$html.css("width", windowWidth);
 	}
+
+	// if (command === 'open') {
+	// 	if($body.css("position") === "absolute") {
+	// 		$body.css("right", "350px");
+	// 	} else if($body.css("position") === "relative") {
+	// 		$body.css("right", "350px");
+	// 	} else if($body.css("width") === ($(window).width() + "px")){
+	// 		$body.css("margin-right", "350px");
+	// 		$body.css("width", "auto");
+	// 	} else {
+	// 		$body.css("margin-right", "350px");
+	// 	}
+	// } else if (command === 'close') {
+	// 	if($body.css("position") === "absolute") {
+	// 		$body.css("right", "0");
+	// 	} else if($body.css("position") === "relative") {
+	// 		$body.css("right", "0");
+	// 	} else if($body.css("width") === "auto"){
+	// 		$body.css("margin-right", "0");
+	// 		$body.css("width", "100%");
+	// 	} else {
+	// 		$body.css("margin-right", "auto");
+	// 	}
+	// }
 }
 
 $(document).on("click", "#minimize-button", function() {
