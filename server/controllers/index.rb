a396028332cd64fbf3b6e41aafaa9052bf7dacd3
Pete Brooks <peterossbrooks@gmail.com>
2014-09-30 15:36:00 -0500

    functional

--- server/controllers/index.rb
+++ server/controllers/index.rb
@@ -3,6 +3,20 @@ get '/?' do
 end
 
 get '/dev' do
+  redirect '/dev/errors'
+end
+
+get '/dev/errors' do
+  @errors = PageError.where(resolved?: false)
+  erb :dev
+end
+
+get '/dev/errors/complete' do
+  @errors = PageError.where(resolved?: true)
+  erb :dev
+end
+
+get '/dev/errors/all' do
   @errors = PageError.all
   erb :dev
 end
